d99cb716025161f9b0b247141363155db7e7eeac
harveersingh-git <somnathpaik@virtualemployee.com>
2022-11-15 13:46:39 +0530

    added min and max length passwords in sign up time - DONE

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -56,7 +56,7 @@ class RegisterController extends Controller
             'first_name' => ['required', 'string', 'max:255'],
             'last_name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'password' => ['required', 'string', 'min:6', 'max:15', 'confirmed'],
         ]);
 
         if($data['role'] != 'employee' && $data['role'] != 'employer'){
