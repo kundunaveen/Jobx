a1be5590fed07d0d2466c771b3587f97c82449b7
harveersingh-git <somnathpaik@virtualemployee.com>
2022-11-15 16:42:43 +0530

    new filesystem url added

--- config/filesystems.php
+++ config/filesystems.php
@@ -51,14 +51,14 @@ return [
         've-dev' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL').'/public/storage',
             'visibility' => 'public',
         ],
 
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/public/storage',
+            'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
         ],
 
