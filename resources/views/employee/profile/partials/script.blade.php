35771667b65f9dc91fe0d97199b1f9d2ce2899d4
Somnath Paik <somnathpaik@virtualemployee.com>
2022-12-07 15:02:18 +0530

    Implemented autocompletion based on job vacancy searches across the entire website - DONE

--- resources/views/employee/profile/partials/script.blade.php
+++ resources/views/employee/profile/partials/script.blade.php
@@ -1,3 +1,7 @@
+<script>
+    var vacancy_search_keyword_route = "{{ route('ajax.autocomplete-search.vacancy') }}";
+    var vacancy_search_location_route = "{{ route('ajax.autocomplete-search.location') }}";
+</script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.13.2/jquery-ui.min.js" integrity="sha512-57oZ/vW8ANMjR/KQ6Be9v/+/h6bq9/l3f0Oc7vn6qMqyhvPd1cvKBRWWpzu0QoneImqr2SkmO4MSqU+RpHom3Q==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
@@ -15,6 +19,8 @@ crossorigin="anonymous"></script>
 <script src="{{asset('js/custom-dev.js')}}"></script>
 
 <script>
+    var vacancy_search_keyword_route = "{{ route('ajax.autocomplete-search.vacancy') }}";
+    var vacancy_search_location_route = "{{ route('ajax.autocomplete-search.location') }}";
 $(document).ready(function(){
     $('.languages').select2()
     $('.skills').select2()
