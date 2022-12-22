873883293e873d01fa15359658e84b5c4ef2a31c
kirtidve <kirtideshwal@virtualemployee.com>
2022-12-22 14:08:59 +0530

    changes

--- resources/views/admin/dashboard/home.blade.php
+++ resources/views/admin/dashboard/home.blade.php
@@ -152,13 +152,15 @@
                                             <span class="designation">{{ optional($new_application->user)->profile_current_job_title }}</span>
                                         </article>
                                     </div>
+                                    @if($applicant_id = optional($new_application->loadMissing('user')->user)->id)
                                     <div class="phone-email-wrapper">
-                                        <a href="{{ route('admin.editEmployee', optional($new_application->loadMissing('user')->user)->id) }}">
+                                        <a href="{{ route('admin.editEmployee', $applicant_id) }}">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" width="22px" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                                         <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
                                                     </svg>
                                         </a>
                                     </div>
+                                    @endif
                                 </li>
                                 @empty
                                 <li class="list-group-item d-flex align-items-center justify-content-between ">
